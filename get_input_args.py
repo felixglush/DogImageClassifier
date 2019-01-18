f6c300528ae42664b00eef7ddd1544be4b202f8d
Felix Glushchenkov <felixglush@gmail.com>
2019-01-18 10:35:07 -0500

    Fixed default image directory from /pet_images to pet_images/ in get_input_args.py

--- get_input_args.py
+++ get_input_args.py
@@ -38,11 +38,11 @@ def get_input_args():
     Returns:
      parse_args() -data structure that stores the command line arguments object  
     """
-    # Create Parse using ArgumentParser
+    # Create Parser using ArgumentParser
     parser = argparse.ArgumentParser()
     
-    # Create 3 command line arguments as mentioned above using add_argument() from ArguementParser method
-    parser.add_argument("--dir", type = str, default="/pet_images", help = "path to the folder of pet images")
+    # Create 3 command line arguments as mentioned above using add_argument() from ArgumentParser method
+    parser.add_argument("--dir", type = str, default="pet_images/", help = "path to the folder of pet images")
     parser.add_argument("--arch", type = str, default="vgg", help = "CNN model architecture")
     parser.add_argument("--dogfile", type = str, default="dognames.txt", help = "text file name with dog names")
     
